df910e2b5d7fda2903d03b7b541a424b3b2b1c7b
Luis.Paes <79490376+luisbr1@users.noreply.github.com>
2022-05-06 22:19:47 -0300

    routes tracing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,6 +14,8 @@ export class HomePage implements OnInit{
    center = new google.maps.LatLng(-22.489371455524722, -48.546544406633046);
    coordinates: Position;
    private autocomplete = new google.maps.places.AutocompleteService();
+   private directions = new google.maps.DirectionsService();
+   private directionsRender = new google.maps.DirectionsRenderer();
 
    listaEnderecos = [];
 
@@ -64,5 +66,34 @@ export class HomePage implements OnInit{
       this.listaEnderecos = listaLocais;
     });
   }
+
+  public tracarRota(local: google.maps.places.AutocompletePrediction){
+    this.listaEnderecos = [];
+    new google.maps.Geocoder().geocode({address: local.description},(resultado) =>{
+      const marker = new google.maps.Marker({
+        position: resultado[0].geometry.location,
+        title: resultado[0].formatted_address,
+        animation: google.maps.Animation.DROP,
+        map: this.map
+      });
+
+      const rota: google.maps.DirectionsRequest = {
+        origin: this.center,
+        destination: resultado[0].geometry.location,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        travelMode: google.maps.TravelMode.DRIVING
+      }
+
+      this.directions.route(rota,(result, status) =>{
+        if(status == 'OK'){
+          this.directionsRender.setMap(this.map);
+          this.directionsRender.setOptions({suppressMarkers: true});
+          this.directionsRender.setDirections(result);
+        }
+      })
+
+    });
+  }
+
 
 }
